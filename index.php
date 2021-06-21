1cd3f410be2d84a24322b11115d0aa2c91a8466f
AndreiBurbulia <77996991+AndreiBurbulia@users.noreply.github.com>
2021-06-21 15:42:53 +0200

    Aggiunta censura parola

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
+$paragrafo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore laudantium molestiae eligendi assumenda recusandae quae, id voluptatem. Temporibus sint quidem corrupti in quas distinctio beatae odio expedita iste numquam tenetur neque, nemo voluptas maxime iure. Obcaecati, voluptatibus tempora optio libero totam, magni omnis qui numquam voluptas ea eaque, sed nesciunt.";
 
+$lunghezza_paragrafo = strlen($paragrafo);
 
+$prova = $_GET['parola'];
+$nuovo_paragrafo = str_replace($prova, '***', $paragrafo);
 
 ?>
 
@@ -16,5 +20,10 @@
 </head>
 <body>
     <h1>Funzia</h1>
+    <p> <?php echo $paragrafo;?> </p>
+    <p>La lunghezza del paragrafo è : <?php echo $lunghezza_paragrafo;?> caratteri</p>
+    <p>Parola da censurare: <?php echo $prova ?></p>
+    <p>Nuovo Paragrafo censurato:</p>
+    <p><?php echo $nuovo_paragrafo?></p>
 </body>
 </html>
